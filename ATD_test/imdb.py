50ef236933d1288ecc617b68e0e47db5a15a6ca8
ankur <ankurj630@gmail.com>
2016-07-04 14:15:37 +0530

    imdb next episode info added

--- ATD_test/imdb.py
+++ ATD_test/imdb.py
@@ -70,7 +70,7 @@ class IMDB:
         connection.close()
         return seasons[0].get_text()
 
-    def get_latest_episode(self,title):
+    def get_latest_episode(self, title):
         self.title = title
         link = self.search_title()
         episode_link = self.parse_link_to_episodes_link(link=link)
@@ -78,4 +78,19 @@ class IMDB:
 
         return self.title_header, self.season, self.age
 
+    def get_next_episode(self, title):
+        search_title = str(title).replace(' ', '-')
+        url = 'https://www.episodate.com/tv-show/' + search_title
+        try:
+            connection = requests.get(url)
+        except requests.RequestException as e:
+            raise SystemExit('\n' + str(e))
 
+        soup = BeautifulSoup(connection.content, 'html5lib')
+        next_episode = soup.find_all('div', {'class': 'col-md-6'})
+        if next_episode[1].find('b', {'class': 'color-green'}):
+            details = next_episode[1].find('b', {'class': 'color-green'})
+            date = next_episode[1].find('b', {'class': 'episode_datetime_convert'})
+            return details.get_text(), date.get_text()
+        else:
+            return None
